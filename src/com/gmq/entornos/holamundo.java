7b1fb00ab7d56e9ff6328974562131f59254435c
Manuel <manuemarb@gmail.com>
2020-01-15 20:22:16 +0100

    he añadido tal

--- src/com/gmq/entornos/holamundo.java
+++ src/com/gmq/entornos/holamundo.java
@@ -1,9 +1,19 @@
 package com.gmq.entornos;
 
+import java.util.Scanner;
+
 public class holamundo {
 	public static void main(String[] main) {
 		System.out.println("Hola mundo");
 		System.out.println("heyyy");
 		System.out.println("holi");
+		
+		int x;
+				
+		Scanner s= new Scanner(System.in);
+		
+		x=s.nextInt();
+		
+		s.close();
 	}
 }
