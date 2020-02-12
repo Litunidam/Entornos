16428b5f1f4dde2e1f160065d2e30745d6fa979b
Manuel <manuemarb@gmail.com>
2020-02-12 20:39:54 +0100

    todo bien

--- src/com/gmq/entornos/EnvironmentCalculator.java
+++ src/com/gmq/entornos/EnvironmentCalculator.java
@@ -22,7 +22,11 @@ public class EnvironmentCalculator implements Calculator {
 
 	@Override
 	public BigDecimal divide(BigDecimal dividend, BigDecimal divisor) {
+		if (divisor == BigDecimal.ZERO) {
+			return BigDecimal.valueOf(Math.E);
+		}
 		return dividend.divide(divisor);
+
 	}
 
 	@Override
@@ -30,24 +34,33 @@ public class EnvironmentCalculator implements Calculator {
 		BigDecimal max = first.max(second);
 		BigDecimal min = first.min(second);
 		BigDecimal mcd = new BigDecimal(0);
+		if (BigDecimal.valueOf(0) == first || BigDecimal.valueOf(0) == second) {
+			System.out.println("holi");
+			return BigDecimal.ZERO;
+		} else {
+			do {
+				mcd = min;
+				min = max.remainder(min);
+				max = mcd;
+			} while (min.compareTo(BigDecimal.ZERO) != 0);
 
-		do {
-			mcd = min;
-			min = max.remainder(min);
-			max = mcd;
-		} while (min.compareTo(BigDecimal.ZERO) != 0);
-		return mcd;
-
+			return mcd;
+		}
 
 	}
 
 	@Override
 	public BigDecimal mcm(BigDecimal first, BigDecimal second) {
-		BigDecimal max = first.max(second);
-		BigDecimal min = first.max(first);
-		BigDecimal mcm = new BigDecimal(0);
-		mcm=(max.divide(this.mcd(first, second))).multiply(min);
-		
-		return mcm;
+		if (BigDecimal.valueOf(0) == first || BigDecimal.valueOf(0) == second) {
+			System.out.println("holi");
+			return BigDecimal.ZERO;
+		} else {
+			BigDecimal max = first.max(second);
+			BigDecimal min = first.max(first);
+			BigDecimal mcm = new BigDecimal(0);
+			mcm = (max.divide(this.mcd(first, second))).multiply(min);
+
+			return mcm;
+		}
 	}
 }
--- test/com/gmq/entornos/EnvironmentCalculatorTest.java
+++ test/com/gmq/entornos/EnvironmentCalculatorTest.java
@@ -3,59 +3,121 @@ package com.gmq.entornos;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class EnvironmentCalculatorTest {
 
-	EnvironmentCalculator calculator = new EnvironmentCalculator();
+	EnvironmentCalculator calculator;
 	
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() {
+		calculator = new EnvironmentCalculator();
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
-	@Test
-	void should_sum_two_variables() {
-		BigDecimal imputx =new BigDecimal(10);
-		BigDecimal imputy = new BigDecimal(5);
-		BigDecimal result;
+	
+	@ParameterizedTest
+	@MethodSource("sumC")
+	void should_sum_two_variables(BigDecimal firstSummand,BigDecimal secondSummand,BigDecimal solution) {
 		//Suma
-		result=new BigDecimal(15);
-		BigDecimal sumResult;
-		sumResult=calculator.sum(imputx, imputy);
-		assertEquals(result,sumResult);
-		//Resta
-		result=new BigDecimal(5);
-		BigDecimal subsResult;
-		subsResult=calculator.substract(imputx, imputy);
-		assertEquals(result,subsResult);
-		//Multiplicación
-		result=new BigDecimal(50);
-		BigDecimal multResult;
-		multResult=calculator.multiply(imputx, imputy);
-		assertEquals(result,multResult);
-		//Division
-		result=new BigDecimal(2);
-		BigDecimal divResult;
-		divResult=calculator.divide(imputx, imputy);
-		assertEquals(result,divResult);
-		//Máximo Común Divisor
-		result=new BigDecimal(5);
-		BigDecimal mcdResult;
-		mcdResult=calculator.mcd(imputx, imputy);
-		assertEquals(result,mcdResult);
-		//Mínimo común Multiplo
-		result=new BigDecimal(5);
-		BigDecimal mcmResult;
-		mcmResult=calculator.mcm(imputx, imputy);
-		assertEquals(result,mcmResult);
-		
+		BigDecimal result=calculator.sum(firstSummand, secondSummand);
+		assertEquals(result,solution);
+	
 	}
-
+	@ParameterizedTest
+	@MethodSource("substrC")
+	void should_substract_two_variables(BigDecimal minuend,BigDecimal substrahend,BigDecimal solution) {
+			BigDecimal result=calculator.substract(minuend, substrahend);
+			assertEquals(result,solution);
+	}
+	
+	@ParameterizedTest
+	@MethodSource("multC")
+	void should_multiply_two_variables(BigDecimal multiplicand,BigDecimal multiplier,BigDecimal solution) {
+			BigDecimal result=calculator.multiply(multiplicand, multiplier);
+			assertEquals(result,solution);
+	}
+	@ParameterizedTest
+	@MethodSource("divC")
+	void should_divide_two_variables(BigDecimal dividend, BigDecimal divisor,BigDecimal solution) {
+			BigDecimal result=calculator.divide(dividend, divisor);
+			assertEquals(result,solution);
+	}
+	@ParameterizedTest
+	@MethodSource("mcdC")
+	void should_mcd_two_variables(BigDecimal first, BigDecimal second,BigDecimal solution) {
+		BigDecimal result=calculator.mcd(first, second);
+		assertEquals(result,solution);
+	}
+	@ParameterizedTest
+	@MethodSource("mcmC")
+	void should_mcm_two_variables(BigDecimal first, BigDecimal second,BigDecimal solution) {
+			BigDecimal result=calculator.mcm(first, second);
+			assertEquals(result,solution);
+	}
+	
+	static Stream<Arguments> sumC(){
+		return Stream.of(
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(5),BigDecimal.valueOf(15))
+		);
+	}
+	static Stream<Arguments> substrC(){
+		return Stream.of(
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(5),BigDecimal.valueOf(5)),
+			Arguments.of(BigDecimal.valueOf(0),BigDecimal.valueOf(0),BigDecimal.valueOf(0)),
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(100),BigDecimal.valueOf(-90)),
+			Arguments.of(BigDecimal.valueOf(-10),BigDecimal.valueOf(100),BigDecimal.valueOf(-110))
+		);
+	}
+	static Stream<Arguments> multC(){
+		return Stream.of(
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(5),BigDecimal.valueOf(50)),
+			Arguments.of(BigDecimal.valueOf(0),BigDecimal.valueOf(0),BigDecimal.valueOf(0)),
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(100),BigDecimal.valueOf(1000)),
+			Arguments.of(BigDecimal.valueOf(-10),BigDecimal.valueOf(100),BigDecimal.valueOf(-1000))
+		);
+	}
+	static Stream<Arguments> divC(){
+		return Stream.of(
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(5),BigDecimal.valueOf(2)),
+			Arguments.of(BigDecimal.valueOf(0),BigDecimal.valueOf(0),BigDecimal.valueOf(Math.E)),
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(100),BigDecimal.valueOf(0.1)),
+			Arguments.of(BigDecimal.valueOf(-100),BigDecimal.valueOf(10),BigDecimal.valueOf(-10)),
+			Arguments.of(BigDecimal.valueOf(1908273),BigDecimal.valueOf(0),BigDecimal.valueOf(Math.E))
+				
+				
+		);
+	}
+	static Stream<Arguments> mcdC(){
+		return Stream.of(
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(5),BigDecimal.valueOf(5)),
+			Arguments.of(BigDecimal.valueOf(0),BigDecimal.valueOf(0),BigDecimal.valueOf(0)),
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(100),BigDecimal.valueOf(10)),
+			Arguments.of(BigDecimal.valueOf(-100),BigDecimal.valueOf(10),BigDecimal.valueOf(10)),
+			Arguments.of(BigDecimal.valueOf(1908273),BigDecimal.valueOf(0),BigDecimal.valueOf(0))
+				
+				
+		);
+	}
+	static Stream<Arguments> mcmC(){
+		return Stream.of(
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(5),BigDecimal.valueOf(10)),
+			Arguments.of(BigDecimal.valueOf(0),BigDecimal.valueOf(0),BigDecimal.valueOf(0)),
+			Arguments.of(BigDecimal.valueOf(10),BigDecimal.valueOf(100),BigDecimal.valueOf(10)),
+			Arguments.of(BigDecimal.valueOf(-100),BigDecimal.valueOf(10),BigDecimal.valueOf(1)),
+			Arguments.of(BigDecimal.valueOf(1908273),BigDecimal.valueOf(0),BigDecimal.valueOf(0))
+				
+				
+		);
+	}
 }
