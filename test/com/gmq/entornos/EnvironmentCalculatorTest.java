c347b1d8b5d077e34fcd0c1b15b55ad5f05eec62
Lituni <manuelmarb@gmail.com>
2020-02-19 18:51:16 +0100

    Actualización paquetes

--- test/com/gmq/entornos/EnvironmentCalculatorTest.java
+++ test/com/gmq/entornos/EnvironmentCalculatorTest.java
@@ -1,16 +1,16 @@
 package com.gmq.entornos;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
 import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+
 
 class EnvironmentCalculatorTest {
 
